eacd5fa116ba2a1c2f4da4cda17ffa3630c463be
Keith Tan <skeithtan@gmail.com>
2018-10-06 13:29:33 +0800

    Add global theming

---
+++ src/app/theme.ts
@@ -0,0 +1,27 @@
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: {
+            main: deepPurple["800"],
+        },
+        secondary: deepPurple,
+    },
+    typography: {
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(","),
+    },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import { Provider } from "mobx-react";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app";
+import theme from "./app/theme";
 import registerServiceWorker from "./registerServiceWorker";
 
 const app = (
     <CssBaseline>
-        <Provider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
+        <MuiThemeProvider theme={theme}>
+            <Provider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+        </MuiThemeProvider>
     </CssBaseline>
 );
 
