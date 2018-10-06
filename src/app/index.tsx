1fc38416ea85e10a11b3c29b5795bf76b482ffcc
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-06 14:33:19 +0800

    Add app bar and temp page content

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,27 @@
+import { withStyles } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import * as React from "react";
+import LyveAppBar from "./components/universal/LyveAppBar";
+import IStyleClasses from "./interfaces/style_classes";
+import styles from "./styles";
 
-export default class App extends React.Component {
+interface IPropsType {
+    classes: IStyleClasses;
+}
+
+class App extends React.Component<IPropsType> {
     public render() {
-        return <h1>Hello World</h1>;
+        const { classes } = this.props;
+        return (
+            <Grid container direction="column" className={classes.root}>
+                <Grid item>
+                    <LyveAppBar />
+                </Grid>
+                <Grid item xs className={classes.page}>
+                    <h1>Current page goes here</h1>
+                </Grid>
+            </Grid>
+        );
     }
 }
+export default withStyles(styles)(App);
