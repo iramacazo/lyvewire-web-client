07c506d6dbd3ac2410e6a29c2132366c519336da
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-06 14:32:05 +0800

    Create app bar title

---
+++ src/app/components/universal/LyveAppBar/components/AppBarTitle/index.tsx
@@ -0,0 +1,8 @@
+import Typography from "@material-ui/core/Typography";
+import * as React from "react";
+
+export default class AppBarTitle extends React.Component {
+    public render() {
+        return <Typography variant="title">Lyvewire</Typography>;
+    }
+}
