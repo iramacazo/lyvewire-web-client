69867e8eb0e92b7963a5740e258ebd214ffceb48
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-07 23:31:48 +0800

    Add temporary stream thumbnail item map

--- src/app/pages/HomePage/components/StreamThumbnails/index.tsx
+++ src/app/pages/HomePage/components/StreamThumbnails/index.tsx
@@ -1,5 +1,6 @@
 import Grid from "@material-ui/core/Grid";
 import * as React from "react";
+import { streamThumbnailItems } from "../../../../services/home_page";
 import StreamThumbnailItem from "../StreamThumbnailItem";
 import StreamThumbnailSection from "../StreamThumbnailSection";
 
@@ -9,9 +10,16 @@ export default class StreamThumbnails extends React.Component {
             <Grid container direction="column" spacing={40}>
                 <Grid item>
                     <StreamThumbnailSection title="Lyve right now">
-                        <StreamThumbnailItem title="Something" />
-                        <StreamThumbnailItem title="Something" />
-                        <StreamThumbnailItem title="Something" />
+                        {streamThumbnailItems.map(sti => {
+                            return (
+                                <StreamThumbnailItem
+                                    key={sti.id}
+                                    title={sti.title}
+                                    description={sti.description}
+                                    imageUrl={sti.imageUrl}
+                                />
+                            );
+                        })}
                     </StreamThumbnailSection>
                 </Grid>
             </Grid>
