0226c289e7396fb189007218afaa6a7f8b9fe3a2
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-13 15:35:42 +0800

    Add stream questions

---
+++ src/app/pages/StreamingPage/components/StreamQuestions/styles.ts
@@ -0,0 +1,11 @@
+import { StyleRules, Theme } from "@material-ui/core/styles";
+
+export default (theme: Theme): StyleRules => ({
+    box: {
+        width: "100%",
+    },
+    root: {
+        height: "100%",
+        width: "100%",
+    },
+});
