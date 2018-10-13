9f29f8a57e839e3706de4f44e8b8438d426b177d
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-13 15:18:28 +0800

    Redesign streaming sidebar

--- src/app/pages/StreamingPage/components/StreamingSidebar/index.tsx
+++ src/app/pages/StreamingPage/components/StreamingSidebar/index.tsx
@@ -1,53 +1,55 @@
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import { inject, observer } from "mobx-react";
 import * as React from "react";
+import StreamSidebarController from "src/app/controllers/stream_sidebar";
 import IStyleClasses from "../../../../interfaces/style_classes";
+import SidebarSection from "../../../../models/enums/sidebar_section";
+import { StreamState } from "../../../../store/stream";
+import StreamChat from "../StreamChat";
+import StreamQuestions from "../StreamQuestions";
 import styles from "./styles";
 
 interface IPropsType {
     classes: IStyleClasses;
+    stream?: StreamState;
 }
 
+@inject("stream")
+@observer
 class StreamingSidebar extends React.Component<IPropsType> {
-    public state = {
-        expanded: "chat",
-    };
-
-    public handleChange = (panel: string) => () => {
-        this.setState({
-            expanded: panel,
-        });
-    };
+    public onTabChange = (event: React.ChangeEvent, value: SidebarSection) =>
+        StreamSidebarController.setActiveTab(value);
 
     public render() {
-        const { expanded } = this.state;
-        const { classes } = this.props;
-
+        const { classes, stream } = this.props;
+        const { activeSidebarTab } = stream!;
         return (
             <Grid container className={classes.root} direction="column">
-                <Grid
-                    item
-                    className={classes.section}
-                    xs={expanded === "questions"}
-                    onClick={this.handleChange("questions")}
-                >
-                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                        <Typography variant="title">Questions</Typography>
-                    </ExpansionPanelSummary>
-                    <h1>Questions content</h1>
+                <Grid item className={classes.tabBarContainer}>
+                    <Tabs
+                        value={activeSidebarTab}
+                        fullWidth
+                        onChange={this.onTabChange}
+                        indicatorColor="secondary"
+                        textColor="secondary"
+                    >
+                        <Tab
+                            label="Questions"
+                            value={SidebarSection.Question}
+                        />
+                        <Tab label="Chat" value={SidebarSection.Chat} />
+                    </Tabs>
+                    <Divider />
                 </Grid>
-                <Grid
-                    item
-                    className={classes.section}
-                    xs={expanded === "chat"}
-                    onClick={this.handleChange("chat")}
-                >
-                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                        <Typography variant="title">Chat</Typography>
-                    </ExpansionPanelSummary>
+                <Grid item xs>
+                    {activeSidebarTab === SidebarSection.Question && (
+                        <StreamQuestions />
+                    )}
+                    {activeSidebarTab === SidebarSection.Chat && <StreamChat />}
                 </Grid>
             </Grid>
         );
